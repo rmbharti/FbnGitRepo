1758c97250cc48cc9c771185a58fc9903feb2875
rm_bharti <rm_bharti@yahoo.com>
2017-06-28 15:50:00 +0530

    Initial Commit

---
+++ fbn-config-server/start.bat
@@ -0,0 +1,9 @@
+start java -jar -Dserver.port=9000 -Dspring.application.name=fbn-config-server1 ./target/fbn-config-server-0.0.1-SNAPSHOT.jar
+--start java -jar -Dserver.port=9001 -Dspring.application.name=fbn-config-service2 .\target\fbn-config-service-0.0.1-SNAPSHOT.jar
+
+
+
+
+
+
+
