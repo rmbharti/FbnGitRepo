8eba355a2ff5b8a00894f973b09854be630a50a8
rm_bharti <rm_bharti@yahoo.com>
2017-06-28 15:51:10 +0530

    Initial Commit

---
+++ fbn/src/main/java/com/fbn/App.java
@@ -0,0 +1,13 @@
+package com.fbn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
