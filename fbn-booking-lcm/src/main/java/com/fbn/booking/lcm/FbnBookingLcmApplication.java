276223882212f25bb10e53b6b51873a4990ff639
rm_bharti <rm_bharti@yahoo.com>
2017-07-02 09:38:23 +0530

    Initial Commit

---
+++ fbn-booking-lcm/src/main/java/com/fbn/booking/lcm/FbnBookingLcmApplication.java
@@ -0,0 +1,12 @@
+package com.fbn.booking.lcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbnBookingLcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbnBookingLcmApplication.class, args);
+	}
+}
