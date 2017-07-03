608804a3d7cd793020f695072462be447ef33d33
rm_bharti <rm_bharti@yahoo.com>
2017-07-03 06:36:33 +0530

    Initial Commit

---
+++ fbn-checkin-lcm/src/main/java/com/fbn/lcm/deploymentrules/DeploymentRule.java
@@ -0,0 +1,5 @@
+package com.fbn.lcm.deploymentrules;
+
+public interface DeploymentRule {
+	public boolean execute();
+}
---
+++ fbn-checkin-lcm/src/main/java/com/fbn/lcm/deploymentrules/DummyDeploymentRule.java
@@ -0,0 +1,12 @@
+package com.fbn.lcm.deploymentrules;
+
+import com.fbn.lcm.deploymentengine.DeploymentEngine;
+
+public class DummyDeploymentRule implements DeploymentRule {
+	private static final int max_instance=2;
+	
+	public boolean execute(){
+		if(DeploymentEngine.num_instances >= max_instance) return false;
+		return true;
+	}
+}
